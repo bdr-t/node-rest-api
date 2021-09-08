b6be4bf37df1d60d2bfe024690c659d943a24174
bader <btabeche23@gmail.com>
2021-09-08 09:27:51 +0200

    fix: upload

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,14 +1,20 @@
 
-const uploadMedia = async (req, res) =>{
-    const {mimetype} = req.file
-    try{
-        if(mimetype === 'image/png' || mimetype === 'image/gif' ||  mimetype === 'image/jpg'){
-            res.send({message: 'Succes'})
-        } else{
-            throw new Error('Only allowed to post png, gif or jpg')
+const uploadMedia = async (req, res) => {
+
+    try {
+        if (!req.file) {
+            throw new Error('No file')
+        } else {
+            const { mimetype } = req.file
+            if (mimetype === 'image/png' || mimetype === 'image/gif' || mimetype === 'image/jpg') {
+                res.send({ message: 'Succes' })
+            } else {
+                throw new Error('Only allowed to post png, gif or jpg')
+            }
+
         }
-    } catch(error){
-        res.status(404).json({message: error.message})
+    } catch (error) {
+        res.status(404).json({ message: error.message })
     }
 }
 
